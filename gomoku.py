767373bf355498f3eb46f0a0010616c216e00b32
lilhat <tah.fighter@hotmail.co.uk>
2023-04-24 20:36:44 +0100

    Co-authored-by: 10ssingh15 <10ssingh15@users.noreply.github.com>

--- gomoku.py
+++ gomoku.py
@@ -19,7 +19,7 @@ from misc import winningTest, legalMove
 
 BOARD_SIZE = 11   # size of the board is 11-by-11
 X_IN_A_LINE = 5   # play the standard game with 5 stones in a line
-TIME_OUT = 5      # player must return a move within 5 seconds
+TIME_OUT = 20      # player must return a move within 5 seconds
 
 # an empty class to host the time-out exception
 class TimeOutException(Exception):
